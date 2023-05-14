5f3cf3d71a67b7d3e9328c33d492609d498a5e7f
user <user@usernoMacBook-Air.local>
2023-05-15 08:19:30 +0900

    update nvimtree

--- lua/kosuke/core/keymaps.lua
+++ lua/kosuke/core/keymaps.lua
@@ -46,8 +46,8 @@ keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+keymap.set("n", "<leader>tl", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>th", ":tabp<CR>") -- go to previous tab
 
 -- plugin keymaps
 
--- lua/kosuke/plugins/nvim-tree.lua
+++ lua/kosuke/plugins/nvim-tree.lua
@@ -3,11 +3,23 @@ if not setup then
   return
 end
 
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+  local function opts(desc)
+    return { desc = "uvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.del("n", "<C-t>", { buffer = bufnr })
+  vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
+end
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvimtree.setup({
+  on_attach = on_attach,
   actions = {
     open_file = {
       quit_on_open = true,
