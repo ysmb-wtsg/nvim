0113a4275380cc77cc44673bf60b5959e72cd17e
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-06-28 21:53:58 +0900

    add gitsigns setup

--- lua/kosuke/plugins/gitsigns.lua
+++ lua/kosuke/plugins/gitsigns.lua
@@ -5,4 +5,12 @@ if not setup then
 end
 
 -- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup({
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1,
+    ignore_whitespace = false,
+  },
+})
