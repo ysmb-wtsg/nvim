8a06ff3ec26cb5a09a6758b9fdde9f03afd2e435
user <user@usernoMacBook-Air.local>
2023-07-29 20:37:28 +0900

    update bufferline configuration

--- lua/kosuke/plugins/nvim-bufferline.lua
+++ lua/kosuke/plugins/nvim-bufferline.lua
@@ -10,7 +10,12 @@ bufferline.setup({
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end
   },
   highlights = {
     separator = {
