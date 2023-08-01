e23cc73707f95965cc3a758e91b48d418aded569
user <user@usernoMacBook-Air.local>
2023-08-01 22:57:44 +0900

    update toggleterm keymaps

--- lua/kosuke/plugins/toggleterm.lua
+++ lua/kosuke/plugins/toggleterm.lua
@@ -28,7 +28,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], opts)
   -- when direction is "float", the following is not very effective
   -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
