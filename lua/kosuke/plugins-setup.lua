ff9c4408f0fce4b2b307c26d72a0f5619001568f
user <user@usernoMacBook-Air.local>
2023-08-06 15:09:04 +0900

    Revert "add firenvim"
    This reverts commit a680657873e8f79f6b1c491985fc608b57e20391.

--- lua/kosuke/plugins-setup.lua
+++ lua/kosuke/plugins-setup.lua
@@ -101,12 +101,6 @@ return packer.startup(function(use)
       run = function() vim.fn["mkdp#util#install"]() end,
   })
 
-  -- use nvim in chrome
-  use {
-      'glacambre/firenvim',
-      run = function() vim.fn['firenvim#install'](0) end
-  }
-
   if packer_bootstrap then
     require("packer").sync()
   end
