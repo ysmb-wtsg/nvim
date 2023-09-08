472cc954fb5a334ed629e0c1ec76c7b65dbfd70b
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-09-08 20:07:30 +0900

    add toggle lazygit

--- lua/kosuke/plugins-setup.lua
+++ lua/kosuke/plugins-setup.lua
@@ -107,6 +107,18 @@ return packer.startup(function(use)
   -- show diagnostics
   use("folke/trouble.nvim")
 
+  -- toggle lazygit
+  use({
+      "kdheepak/lazygit.nvim",
+      requires = {
+          "nvim-telescope/telescope.nvim",
+          "nvim-lua/plenary.nvim",
+      },
+      config = function()
+          require("telescope").load_extension("lazygit")
+      end,
+  })
+
   use {
     'phaazon/hop.nvim',
     branch = 'v2', -- optional but strongly recommended
