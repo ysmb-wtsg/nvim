6bbedd60153e3121bb96f89c370ad8df3052a68d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-09-27 12:55:13 +0900

    add lazydocker toggle & update keymaps

--- lua/kosuke/plugins/toggleterm.lua
+++ lua/kosuke/plugins/toggleterm.lua
@@ -39,11 +39,11 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
--- local Terminal  = require('toggleterm.terminal').Terminal
--- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
---
--- function _lazygit_toggle()
---   lazygit:toggle()
--- end
---
--- vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, count = 999 })
+
+function _lazydocker_toggle()
+  lazydocker:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua _lazydocker_toggle()<CR>", {noremap = true, silent = true})
