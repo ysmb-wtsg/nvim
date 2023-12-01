0bda37f46c2544436e1c6b1bfb2ed61aa7f995cf
user <user@usernoMacBook-Air.local>
2023-12-02 05:11:21 +0900

    update keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,8 +12,8 @@ end
 
 set_to_multimodes("n,v", "j", "gj")
 set_to_multimodes("n,v", "k", "gk")
--- set_to_multimodes("n,v", "<S-j>", "15j")
--- set_to_multimodes("n,v", "<S-k>", "15k")
+set_to_multimodes("n,v", "<S-j>", "15j")
+set_to_multimodes("n,v", "<S-k>", "15k")
 set_to_multimodes("n,v", "<S-h>", "0")
 set_to_multimodes("n,v", "<S-l>", "$")
 keymap.set("n", "<Tab>", ">>")
@@ -38,12 +38,11 @@ keymap.set("n", "<leader>q", ":q<CR>")
 keymap.set("n", "<leader>fq", ":q!<CR>") -- !=force
 keymap.set("n", "<leader>jj", ":wq<CR>")
 
-keymap.set("n", "<leader>s", ":%s/")
+keymap.set("n", "<leader>rp", ":%s/")
 keymap.set("n", "<leader>i", "gg=<S-g>")
-keymap.set("n", "<leader>r", ":e<CR>")
-keymap.set("n", "<leader>R", ":bufdo e<CR>")
+keymap.set("n", "<leader>rf", ":e<CR>")
 
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>s", "<C-w>v") -- split window vertically
 -- keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 -- keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equil width
 
@@ -82,15 +81,12 @@ keymap.set("i", "：", ":")
 -- plugin keymaps
 
 -- nvim-tree
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
 keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
-keymap.set("n", "<leader>fn", "<cmd>Telescope help_tags<CR>")
 
 -- markdown-preview
 keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>")
