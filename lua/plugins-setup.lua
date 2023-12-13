301482a3922bc76faa178647ef68a1a3063fc867
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-12-13 14:53:50 +0900

    fix auto_session config

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -168,7 +168,7 @@ return packer.startup(function(use)
     config = function()
       require("auto-session").setup {
         log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+        auto_session_suppress_dirs = "~/",
       }
     end
   }
