ea6ce5fe438b0ae8928db54350cb4aa5022568f2
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-12-31 15:12:21 +0900

    tidy plugins-setup

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -23,17 +23,10 @@ if not status then
 end
 
 return packer.startup(function(use)
+	-- packageManager
 	use("wbthomason/packer.nvim")
 
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
-
-	use("FotiadisM/tabset.nvim") -- set indent each language
-
-	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
-
-	use("akinsho/nvim-bufferline.lua") -- tab appearance
-
-	-- colorscheme
+	-- colorScheme
 	use({ "Shatur/neovim-ayu", as = "ayu" })
 	use({ "catppuccin/nvim", as = "catppuccin" })
 	use("rebelot/kanagawa.nvim")
@@ -44,158 +37,45 @@ return packer.startup(function(use)
 		opts = {},
 	})
 
-	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+	use("nvimtools/none-ls.nvim")
+	use({ "nvimdev/lspsaga.nvim", branch = "main" })
+	use("folke/trouble.nvim")
 
-	use("akinsho/toggleterm.nvim") -- toggle terminal
-
-	-- surround
-	use({
-		"kylechui/nvim-surround",
-		branch = "main",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	})
-
-	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
-
-	-- commenting with gc
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup({})
-		end,
-	})
-
-	use("nvim-tree/nvim-tree.lua") -- file explorer
-
-	use("kyazdani42/nvim-web-devicons") -- icons
-
-	use("nvim-lualine/lualine.nvim") -- statusline
-
-	-- fuzzy findings /w telescope
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({ "nvim-telescope/telescope-project.nvim" })
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
-
-	-- autocompletion
+	-- completion
 	use("hrsh7th/nvim-cmp")
+	use("onsails/lspkind.nvim")
 	use("hrsh7th/cmp-nvim-lsp")
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
 
-	-- snippets
+	-- snippet
 	use("L3Mon4D3/LuaSnip")
 	use("saadparwaiz1/cmp_luasnip")
 	use("rafamadriz/friendly-snippets")
 
-	-- managing & installing lsp server
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use({ "nvimdev/lspsaga.nvim", branch = "main" })
-	use("onsails/lspkind.nvim")
-
-	-- configuring lsp server
-	use("neovim/nvim-lspconfig")
-
-	-- formatting & linting
-	use("nvimtools/none-ls.nvim") -- configure formatters & linters
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
-
-	-- treesitter
+	-- ui
+	use("nvim-tree/nvim-tree.lua")
+	use("kyazdani42/nvim-web-devicons")
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
 			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
 	})
-
-	-- auto closing
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
-
-	-- git signs plugin
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			require("scrollbar.handlers.gitsigns").setup()
-		end,
-	})
-
-	-- markdown preview
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
-
-	-- show diagnostics
-	use("folke/trouble.nvim")
-
-	-- hop
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-	})
-
-	-- neo scroll
+	use("akinsho/nvim-bufferline.lua")
+	use("nvim-lualine/lualine.nvim")
 	use({
 		"karb94/neoscroll.nvim",
 		config = function()
 			require("neoscroll").setup({})
 		end,
 	})
-
-	-- scroll bar
 	use("petertriho/nvim-scrollbar")
-
-	-- zenmode
-	use("folke/zen-mode.nvim")
-
-	-- easy action
-	use({
-		"Weissle/easy-action",
-		requires = {
-			{
-				"kevinhwang91/promise-async",
-				module = { "async" },
-			},
-		},
-	})
-
-	-- session manager
-	use({
-		"rmagatti/auto-session",
-		config = function()
-			require("auto-session").setup({
-				log_level = "error",
-				auto_session_suppress_dirs = "~/",
-			})
-		end,
-	})
-
-	-- chatGPT
-	use({
-		"jackMort/ChatGPT.nvim",
-		config = function()
-			require("chatgpt").setup()
-		end,
-		requires = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-	})
-
-	-- smart selection
-	use("gcmt/wildfire.vim")
-
-	-- note taking
-	use("gsuuon/note.nvim")
-
-	-- noice
 	use({
 		"folke/noice.nvim",
 		-- config = function()
@@ -208,6 +88,85 @@ return packer.startup(function(use)
 			"rcarriga/nvim-notify",
 		},
 	})
+	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+
+	-- search
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope-project.nvim" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			require("scrollbar.handlers.gitsigns").setup()
+		end,
+	})
+
+	-- util
+	use("nvim-lua/plenary.nvim")
+	use("FotiadisM/tabset.nvim")
+	use("christoomey/vim-tmux-navigator")
+	use("akinsho/toggleterm.nvim")
+	use({
+		"kylechui/nvim-surround",
+		branch = "main",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	})
+	use("inkarkat/vim-ReplaceWithRegister")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({})
+		end,
+	})
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+	use({
+		"phaazon/hop.nvim",
+		branch = "v2",
+	})
+	use("folke/zen-mode.nvim")
+	use({
+		"Weissle/easy-action",
+		requires = {
+			{
+				"kevinhwang91/promise-async",
+				module = { "async" },
+			},
+		},
+	})
+	use({
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = "~/",
+			})
+		end,
+	})
+	use({
+		"jackMort/ChatGPT.nvim",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	})
+	use("gcmt/wildfire.vim")
+	use("gsuuon/note.nvim")
 
 	if packer_bootstrap then
 		require("packer").sync()
