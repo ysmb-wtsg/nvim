858a55f0644db22fb79044eb98bcc64d1f13350d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-05-30 17:24:17 +0900

    add rspsaga

--- lua/settings/plugins/lsp/lspconfig.lua
+++ lua/settings/plugins/lsp/lspconfig.lua
@@ -24,29 +24,6 @@ return {
 				-- Buffer local mappings.
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
-
-				-- set keybinds
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "gk", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rl", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 			end,
---
+++ lua/settings/plugins/lsp/lspsaga.lua
@@ -0,0 +1,19 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
+	config = function()
+		local opts = { noremap = true, silent = true }
+		require("lspsaga").setup({
+			vim.keymap.set("n", "<leader>lf", "<cmd>Lspsaga finder def+ref ++layout<CR>", opts),
+			vim.keymap.set("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", opts),
+			vim.keymap.set("n", "<leader>lo", "<cmd>Lspsaga outline<CR>", opts),
+			vim.keymap.set("n", "<leader>lr", "<cmd>Lspsaga incoming_calls<CR>", opts),
+			vim.keymap.set("n", "<leader>ld", "<cmd>Lspsaga outgoing_calls<CR>", opts),
+			vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts),
+			vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts),
+		})
+	end,
+}
