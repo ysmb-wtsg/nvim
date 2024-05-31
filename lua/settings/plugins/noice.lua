63949ea10912407b1cb5fc488a25e5123ad88f5a
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-05-31 11:27:41 +0900

    add noice

---
+++ lua/settings/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/noice.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		routes = {
+			{
+				filter = { event = "notify", find = "No information available" },
+				opts = { skip = true },
+			},
+		},
+		presets = {
+			lsp_doc_border = true,
+		},
+	},
+}
---
+++ lua/settings/plugins/notify.lua
@@ -0,0 +1,7 @@
+return {
+	"rcarriga/nvim-notify",
+	opts = {
+		timeout = 1000,
+		stages = "fade_in_slide_out",
+	},
+}
