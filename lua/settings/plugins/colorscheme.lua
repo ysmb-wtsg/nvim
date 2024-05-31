259c99160c685d181a9614cf8ea1afb06b46201d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-05-31 14:24:23 +0900

    add colorthemes

--- lua/settings/plugins/colorscheme.lua
+++ lua/settings/plugins/colorscheme.lua
@@ -1,6 +1,26 @@
-return {
-	"Shatur/neovim-ayu",
-	config = function()
-		vim.cmd("colorscheme ayu-dark")
-	end,
+local themes = {
+	ayu = {
+		"Shatur/neovim-ayu",
+		lazy = false,
+		config = function()
+			vim.cmd("colorscheme ayu-dark")
+		end,
+	},
+	tokyonight = {
+		"folke/tokyonight.nvim",
+		lazy = false,
+		config = function()
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
+	catppuccin = {
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		config = function()
+			vim.cmd("colorscheme catppuccin-mocha")
+		end,
+	},
 }
+
+return themes.tokyonight
