be7f712addbf09356d29e5c28681bc553f4cfe04
user <user@MacBook-Air.local>
2024-06-04 00:50:10 +0900

    add hover

--- lua/settings/plugins/lsp/lspsaga.lua
+++ lua/settings/plugins/lsp/lspsaga.lua
@@ -9,7 +9,6 @@ return {
 		require("lspsaga").setup({
 			vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts),
 			vim.keymap.set("n", "gf", "<cmd>Lspsaga finder def+ref ++layout<CR>", opts),
-			vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", opts),
 			vim.keymap.set("n", "go", "<cmd>Lspsaga outline<CR>", opts),
 			vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts),
 		})
