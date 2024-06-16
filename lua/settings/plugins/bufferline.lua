fba94b6326d6ea8a0d15876490d63ce607164d03
user <user@MacBook-Air.local>
2024-06-17 00:13:24 +0900

    update bufferline settings

--- lua/settings/plugins/bufferline.lua
+++ lua/settings/plugins/bufferline.lua
@@ -5,8 +5,22 @@ return {
 		local bufferline = require("bufferline")
 		bufferline.setup({
 			options = {
+				custom_filter = function(buf_number)
+					if vim.bo[buf_number].filetype ~= "NvimTree" and vim.bo[buf_number].filetype ~= "toggleterm" then
+						return true
+					end
+					return false
+				end,
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "📂NvimTree",
+						separator = true,
+						highlight = "Directory",
+					},
+				},
 				mode = "tabs",
-				separator_style = "thin",
+				separator_style = "slant",
 				always_show_bufferline = true,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
@@ -16,26 +30,6 @@ return {
 					local icon = level:match("error") and " " or " "
 					return " " .. icon .. count
 				end,
-			},
-			highlights = {
-				separator = {
-					guifg = "#073642",
-					guibg = "#002b36",
-				},
-				separator_selected = {
-					guifg = "#073642",
-				},
-				background = {
-					guifg = "#657b83",
-					guibg = "#002b36",
-				},
-				buffer_selected = {
-					guifg = "#fdf6e3",
-					gui = "bold",
-				},
-				fill = {
-					guibg = "#073642",
-				},
 			},
 		})
 	end,
