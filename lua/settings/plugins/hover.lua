91e67731e09fd8bc26f4cbd40d630c154a298cff
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-06-21 10:27:22 +0900

    fix hover keymap

--- lua/settings/plugins/hover.lua
+++ lua/settings/plugins/hover.lua
@@ -29,10 +29,10 @@ return {
 		-- Setup keymaps
 		vim.keymap.set("n", "gk", require("hover").hover, { desc = "Hover doc" })
 		-- vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<C-p>", function()
 			require("hover").hover_switch("previous")
 		end, { desc = "hover.nvim (previous source)" })
-		vim.keymap.set("n", "<C-l>", function()
+		vim.keymap.set("n", "<C-n>", function()
 			require("hover").hover_switch("next")
 		end, { desc = "hover.nvim (next source)" })
 
