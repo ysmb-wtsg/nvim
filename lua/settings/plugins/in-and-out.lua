5c9589aa9f74412a48a093eb4358113deaeb6d5a
user <user@MacBook-Air.local>
2024-07-05 22:52:53 +0900

    in-and-out test

--- lua/settings/plugins/in-and-out.lua
+++ lua/settings/plugins/in-and-out.lua
@@ -1,8 +1,16 @@
 return {
-	"ysmb-wtsg/in-and-out.nvim",
-	config = function()
-		vim.keymap.set("i", "<C-CR>", function()
-			require("in-and-out").in_and_out()
-		end)
-	end,
+	"telemachus/in-and-out.nvim",
+	branch = "config",
+	keys = {
+		{
+			"<C-CR>",
+			function()
+				require("in-and-out").in_and_out()
+			end,
+			mode = "i",
+		},
+	},
+	opts = {
+		additional_targets = { "“", "”" },
+	},
 }
