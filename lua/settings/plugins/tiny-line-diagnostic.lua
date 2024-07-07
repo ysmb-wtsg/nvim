c981c9777a459da9091b372c72f4553cfbbc31ff
user <user@MacBook-Air.local>
2024-07-08 02:35:50 +0900

    add tiny-line-dianostics

---
+++ lua/settings/plugins/tiny-line-diagnostic.lua
@@ -0,0 +1,8 @@
+return {
+	"rachartier/tiny-inline-diagnostic.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("tiny-inline-diagnostic").setup()
+		vim.diagnostic.config({ virtual_text = false })
+	end,
+}
