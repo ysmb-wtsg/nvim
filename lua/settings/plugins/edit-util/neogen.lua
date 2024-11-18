2cc4cce2b85a9cde2389a59bf727d8e65b945298
ysmb-wtsg <telecaster1121@gmail.com>
2024-11-18 21:38:04 +0900

    add neogen

---
+++ lua/settings/plugins/edit-util/neogen.lua
@@ -0,0 +1,8 @@
+return {
+	"danymat/neogen",
+	config = function()
+		require("neogen").setup({ snippet_engine = "luasnip" })
+		vim.keymap.set("n", "dc", "<cmd>Neogen<CR>", { desc = "Generate docstring" })
+	end,
+	version = "*",
+}
