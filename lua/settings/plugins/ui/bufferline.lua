8138c6f3dedac8fa2552762729f3651c5af0d95d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-11-28 11:01:57 +0900

    update bufferline config

--- lua/settings/plugins/ui/bufferline.lua
+++ lua/settings/plugins/ui/bufferline.lua
@@ -24,7 +24,6 @@ return {
 			indicator = {
 				style = "none",
 			},
-			always_show_bufferline = false,
 			show_buffer_close_icons = false,
 			show_close_icon = false,
 			diagnostics = "nvim_lsp",
