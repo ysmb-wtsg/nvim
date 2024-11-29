2f51272d48779a989f0f40a360773e306afe15e7
ysmb-wtsg <telecaster1121@gmail.com>
2024-11-29 23:29:34 +0900

    add codium

--- lua/settings/plugins/edit-util/nvim-cmp.lua
+++ lua/settings/plugins/edit-util/nvim-cmp.lua
@@ -57,14 +57,16 @@ return {
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
-				{ name = "orgmode" },
+				{ name = "codeium" },
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
-				format = lspkind.cmp_format({
+				format = require("lspkind").cmp_format({
+					mode = "symbol",
 					maxwidth = 50,
 					ellipsis_char = "...",
+					symbol_map = { Codeium = "" },
 				}),
 			},
 		})
