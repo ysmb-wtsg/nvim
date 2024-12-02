fe920c6cceff52c7a4e2ddfbb609ecd41491d001
ysmb-wtsg <telecaster1121@gmail.com>
2024-12-03 05:39:44 +0900

    update diacnostics toggler

--- lua/settings/plugins/lsp/lspsaga.lua
+++ lua/settings/plugins/lsp/lspsaga.lua
@@ -1,14 +1,36 @@
 return {
 	"nvimdev/lspsaga.nvim",
-	keys = {
-		{ "gd", "<cmd>Lspsaga goto_definition<CR>", silent = true, desc = "Go to definition" },
-		{ "gf", "<cmd>Lspsaga finder def+ref ++layout<CR>", silent = true, desc = "Show definition and references" },
-		{ "go", "<cmd>Lspsaga outline<CR>", silent = true, desc = "Show outline" },
-		{ "<leader>ca", "<cmd>Lspsaga code_action<CR>", silent = true, desc = "Show available code actions" },
-	},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter", -- optional
 		"nvim-tree/nvim-web-devicons", -- optional
 	},
-	opts = {},
+	config = function()
+		require("lspsaga").setup({})
+		vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { silent = true, desc = "Go to definition" })
+		vim.keymap.set(
+			"n",
+			"gf",
+			"<cmd>Lspsaga finder def+ref ++layout<CR>",
+			{ silent = true, desc = "Show definition and references" }
+		)
+		vim.keymap.set("n", "go", "<cmd>Lspsaga outline<CR>", { silent = true, desc = "Show outline" })
+		vim.keymap.set(
+			"n",
+			"gt",
+			"<cmd>Lspsaga show_buf_diagnostics<CR>",
+			{ silent = true, desc = "Show buffer diagnostics" }
+		)
+		vim.keymap.set(
+			"n",
+			"gT",
+			"<cmd>Lspsaga show_workspace_diagnostics<CR>",
+			{ silent = true, desc = "Show workspace diagnostics" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ca",
+			"<cmd>Lspsaga code_action<CR>",
+			{ silent = true, desc = "Show available code actions" }
+		)
+	end,
 }
