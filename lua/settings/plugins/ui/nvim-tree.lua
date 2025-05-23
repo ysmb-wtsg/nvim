c9684d52d13c0da77313bd039c99a1b2cf31ff04
ikeda-ks <kosuke.ikeda@cleaveware.com>
2025-05-24 05:01:16 +0900

    update nvimtree config

--- lua/settings/plugins/ui/nvim-tree.lua
+++ lua/settings/plugins/ui/nvim-tree.lua
@@ -11,7 +11,7 @@ return {
 			local api = require("nvim-tree.api")
 
 			local function opts(desc)
-				return { desc = "uvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 			end
 
 			api.config.mappings.default_on_attach(bufnr)
@@ -19,6 +19,7 @@ return {
 			vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
 			vim.keymap.set("n", "s", api.node.open.vertical, opts("Open: Vertical Split"))
 			vim.keymap.set("n", "w", api.node.navigate.parent_close, opts("Close Directory"))
+			vim.keymap.set("n", "i", api.node.show_info_popup, opts("Toggle info"))
 		end
 		local nvimtree = require("nvim-tree")
 		vim.g.loaded_netrw = 1
