60db2c26e7cf693a3ad7d607767f01e5a654d038
ysmb-wtsg <telecaster1121@gmail.com>
2025-05-26 22:37:36 +0900

    add gtd.nvim

---
+++ lua/settings/plugins/util/gtd.lua
@@ -0,0 +1,8 @@
+return {
+	"ysmb-wtsg/gtd.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	keys = { { "<leader>tt", "<cmd>GtdOpen<CR>", desc = "Open GTD Modal" } },
+	config = function()
+		require("gtd").setup()
+	end,
+}
