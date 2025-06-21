9c3888537d5bf1e172c57f782d35187468769c25
ysmb-wtsg <telecaster1121@gmail.com>
2025-06-21 21:01:53 +0900

    add trouble

---
+++ lua/settings/plugins/lsp/trouble.lua
@@ -0,0 +1,59 @@
+return {
+	"folke/trouble.nvim",
+	opts = {
+		auto_preview = false,
+		modes = {
+			diagnostics = {
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+			},
+			lsp = {
+				win = {
+					type = "float",
+					relative = "editor",
+					title = "LSP Definitions / references / ... (Trouble)",
+					title_pos = "center",
+				},
+			},
+			symbols = {
+				win = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+			},
+		},
+		keys = {
+			["<leader>"] = "fold_toggle",
+			w = "fold_close",
+		},
+	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"gt",
+			"<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"go",
+			"<cmd>Trouble symbols toggle focus=true<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"gf",
+			"<cmd>Trouble lsp toggle focus=true<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"gq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
+}
