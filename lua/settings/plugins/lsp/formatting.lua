887296110caca0e8cdc0dc4a7c1a16f129b7935d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2025-07-03 07:55:16 +0900

    fix conform options

--- lua/settings/plugins/lsp/formatting.lua
+++ lua/settings/plugins/lsp/formatting.lua
@@ -19,7 +19,7 @@ return {
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
-				python = { "ruff" },
+				python = { "ruff_fix", "ruff_format" },
 				sh = { "shfmt" },
 			},
 			format_on_save = function(bufnr)
