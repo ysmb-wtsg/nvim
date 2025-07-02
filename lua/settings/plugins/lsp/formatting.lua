0a949e6bb1a609b03d09576460949ca7583f296d
ikeda-ks <kosuke.ikeda@cleaveware.com>
2025-07-03 07:47:44 +0900

    :tada: add auto-save toggle command

--- lua/settings/plugins/lsp/formatting.lua
+++ lua/settings/plugins/lsp/formatting.lua
@@ -22,19 +22,46 @@ return {
 				python = { "ruff" },
 				sh = { "shfmt" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
+		})
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>i", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file" })
+			local bufnr = vim.api.nvim_get_current_buf()
+			local current = vim.b[bufnr].disable_autoformat or vim.g.disable_autoformat
+			local new_state = not current
+
+			vim.b[bufnr].disable_autoformat = new_state
+			vim.g.disable_autoformat = new_state
+
+			if new_state then
+				vim.notify("Autoformat disabled", vim.log.levels.INFO)
+			else
+				vim.notify("Autoformat enabled", vim.log.levels.INFO)
+			end
+		end, { desc = "Format Toggle" })
 	end,
 }
