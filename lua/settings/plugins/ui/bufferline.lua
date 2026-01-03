c86267d027fb5900790070f7f5fccad5bc5c136e
ysmb-wtsg <telecaster1121@gmail.com>
2026-01-03 23:37:40 +0900

    update bufferline config

--- lua/settings/plugins/ui/bufferline.lua
+++ lua/settings/plugins/ui/bufferline.lua
@@ -7,6 +7,7 @@ return {
   },
   opts = {
     options = {
+      separator_style = "slant",
       custom_filter = function(buf_number)
         if vim.bo[buf_number].filetype ~= "NvimTree" and vim.bo[buf_number].filetype ~= "toggleterm" then
           return true
