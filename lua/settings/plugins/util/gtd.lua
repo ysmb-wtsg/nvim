ae41325efb2ef6ea301f5fd380ee05f2871fc19e
ysmb-wtsg <telecaster1121@gmail.com>
2026-01-05 22:49:49 +0900

    remove gtd

--- lua/settings/plugins/util/gtd.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ysmb-wtsg/gtd.nvim",
-  dependencies = { "MunifTanjim/nui.nvim" },
-  keys = { { "<leader>tt", "<cmd>GtdOpen<CR>", desc = "Open GTD Modal" } },
-  config = function()
-    require("gtd").setup()
-  end,
-}
